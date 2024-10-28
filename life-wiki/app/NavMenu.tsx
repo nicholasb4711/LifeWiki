390712686010cf5b77f59b7e41d746257ad64828
nicholasb4711 <nicholasb4711@gmail.com>
2024-10-28 14:19:54 -0400

    bootstrap added

--- life-wiki/app/NavMenu.tsx
+++ life-wiki/app/NavMenu.tsx
@@ -1,22 +1,43 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './NavMenu.module.css';
 
 export default function NavMenu() {
     return (
-        <nav className={styles.navMenu}>
-            <Link href={'/'}>
-                <Image src="next.svg" alt="Life Wiki Logo" width={100} height={100} />
-            </Link>
-            <ul className={styles.links}>
-                <li>
-                    <Link href={'/'}>Home</Link>
-                </li>
-                <li>
-                    <Link href={'/pages/about'}>About</Link>
-                </li>
-            </ul>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">LifeWiki</a>
+                
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className='nav-link' href="/pages/about">About</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
         </nav>
-    )
-
+    );
 }
--- life-wiki/app/page.tsx
+++ life-wiki/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./page.module.css";
 
 export default function Home() {
