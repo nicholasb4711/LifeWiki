ec8438513a2295eb47206239ddd9bf679ca93718
nicholasb4711 <byrne.ni@northeastern.edu>
2024-10-29 17:17:19 -0400

    learning next

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function dashboard() {
+    return (
+        <div>
+            <h1>Dashboard Page</h1>
+            {/* Link to home page */}
+            <a href="/">Home</a>
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page1</h1>
+      {/* Link to dashboard page */}
+      <a href="/dashboard">Dashboard</a>
+    </div>
+  )
+}
